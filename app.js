95c13a6da647c2895f7d39a7f4f665d032c5566e
priyanka pamnani <priyankapamnani4@gmail.com>
2025-02-18 17:12:35 +0530

    hello

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ app.use((req,res,next)=>{
     res.locals.curuser=req.user
     next();
 })
-// app.get("/",(req,res)=>{
-//     res.send("working")
-// })
+app.get("/",(req,res)=>{
+    res.redirect("/listings")
+})
 const mongoose=require("mongoose");
 main()
 .then(()=>{
